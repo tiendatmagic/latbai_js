d13b4244265f1447626e60a0c033ff3166091dde
tiendatmagic <tiendatmagic8@gmail.com>
2020-11-09 23:10:23 +0700

    add

---
+++ magic.js
@@ -0,0 +1,43 @@
+var math = Math.ceil(Math.random() * 5);
+var array = [];
+
+
+
+function arrayy() {
+
+    array.push(math);
+    alert(array)
+    for (i = 1; i < 5; i++) {
+        math = Math.ceil(Math.random() * 5);
+        
+        array.push(math);
+        alert(array);
+    }
+}
+
+
+arrayy();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+var myArray = ["Banana", "Orange", "Apple", "Mango"];
+
+if (myArray.indexOf("Bananaa") === -1) {
+  alert("element doesn't exist");
+}
+else {
+  alert("element found");
+}
+
+*/
