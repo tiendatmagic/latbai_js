202e22d122a5f1dfa2c1b4bc71d0d20b81babb57
tiendatmagic <tiendatmagic8@gmail.com>
2020-11-09 23:58:21 +0700

    add

--- magic.js
+++ magic.js
@@ -1,26 +1,27 @@
-var math = Math.ceil(Math.random() * 5);
-var array = [];
+for (var aaa=[],i=0;i<5;++i) aaa[i]=i;
+
+// http://stackoverflow.com/questions/962802#962890
+function shuffle(array) {
+  var tmp, current, top = array.length;
+  if(top) while(--top) {
+    current = Math.floor(Math.random() * (top + 1));
+    tmp = array[current];
+    array[current] = array[top];
+    array[top] = tmp;
+  }
+  return array;
+}
+
+arrr = shuffle(aaa);
+alert(arrr);   
 
 
 
 function arrayy() {
-
-    
-    
-    
-
-
-    for (i = 1; i <= 5; i++) {
-        math = Math.ceil(Math.random() * 5);
-
-        array.push(math);
-        alert(array);
-
-
-
-        //
-        
+    for (i = 0; i <= 10; i++) {
+        document.getElementsByClassName("box")[arrr[0]].style.background='red'; 
     }
+
 }
 
 
@@ -39,13 +40,24 @@ arrayy();
 
 /*
 
-var myArray = ["Banana", "Orange", "Apple", "Mango"];
 
-if (myArray.indexOf("Bananaa") === -1) {
+
+if (array.indexOf(math) === -1) {
   alert("element doesn't exist");
 }
 else {
   alert("element found");
 }
+
+
+
+function GetRandomNum(Min,Max)
+{
+var Range = Max - Min;
+var Rand = Math.random();
+return(Min + Math.round(Rand * Range));
+}
+var num = GetRandomNum(1,5);
+alert(num);
 
 */
